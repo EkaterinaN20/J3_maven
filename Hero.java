460aae81279443b9ce00f66f53ffe27dd1358990
EkaterinaN20 <katni@mail.ru>
2020-02-29 17:28:22 +0300

    My first game

---
+++ Hero.java
@@ -0,0 +1,35 @@
+package com.geekbrains.rpg.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Hero {
+    private Texture texture;
+    private Vector2 position;
+    private float speed;
+
+    public Hero() {
+        this.texture = new Texture("hero.png");
+        this.position = new Vector2(100, 100);
+        this.speed = 100.0f;
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x - 32, position.y - 32, 32, 32, 64, 64, 1, 1, 0, 0, 0, 64, 64, false, false);
+    }
+
+    public void update(float dt) {
+
+        if(Gdx.input.getX() > position.x)
+            position.x += speed * dt;;
+        if (Gdx.input.getX() < position.x)
+            position.x -= speed * dt;
+        if(720.0f - Gdx.input.getY() > position.y)
+            position.y += speed * dt;;
+        if (720.0f - Gdx.input.getY() <position.y)
+            position.y -= speed * dt;
+    }
+}
